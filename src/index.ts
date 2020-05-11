476a29ae61385ed4f835254142eb482c8f304a4a
Erny Sans <ernysans@icloud.com>
2020-05-11 15:20:02 -0500

    deleted status

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ import * as linkDomain from "./link-domain";
 import pubSubEvent from "./pubsub-event";
 import replaceMessageText from "./replace-message-text";
 import specialCharToRegular from "./special-char-to-regular";
-// import * as status from "./status";
 import * as strings from "./strings";
 import validateUrl from "./validate-url";
 
@@ -32,7 +31,6 @@ export {
   pubSubEvent,
   replaceMessageText,
   specialCharToRegular,
-  // status,
   strings,
   validateUrl,
 };
