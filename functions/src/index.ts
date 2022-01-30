5805e787586558d787000c0700990ddc7be7b1e3
Erny Sans <ernysans@icloud.com>
2022-01-30 17:33:22 -0500

    Dep udate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,18 +2,18 @@
  * @license
  * Copyright FabricElements. All Rights Reserved.
  */
-// import * as admin from 'firebase-admin';
-import {getApps, initializeApp} from 'firebase-admin/app';
+import * as admin from 'firebase-admin';
+// import {getApps, initializeApp} from 'firebase-admin/app';
 
 /**
  * Init firebase app first
  */
-// if (!admin.apps.length) {
-//   admin.initializeApp();
-// }
-if (!getApps.length) {
-  initializeApp();
+if (!admin.apps.length) {
+  admin.initializeApp();
 }
+// if (!getApps.length) {
+//   initializeApp();
+// }
 /**
  * Export app modules after the app is initialized
  */
